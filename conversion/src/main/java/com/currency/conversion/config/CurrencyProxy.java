347b6502764ce8fefbb5f7286c85c6b494ee8e88
sayantan20 <sayantanmitra20@gmail.com>
2021-06-24 08:49:54 +0530

    target/classes/application.properties

--- conversion/src/main/java/com/currency/conversion/config/CurrencyProxy.java
+++ conversion/src/main/java/com/currency/conversion/config/CurrencyProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "exchange-service", url = "localhost:9090")
+@FeignClient(name = "exchange-service")
 public interface CurrencyProxy {
     @GetMapping("/currency/{from}/to/{to}")
     CurrencyConversion currencyConversion(@PathVariable("from") String from, @PathVariable("to") String to);
