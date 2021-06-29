cae824b55d2ae1f8bb411c4e6eecd3bb47adcd5f
sayantan20 <sayantanmitra20@gmail.com>
2021-06-29 18:39:22 +0530

    ConfigServerApplication

--- config-server/src/main/java/com/currency/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/currency/configserver/ConfigServerApplication.java
@@ -1,9 +1,10 @@
 package com.currency.configserver;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+//@EnableEncryptableProperties
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
---
+++ config-server/src/main/java/com/currency/configserver/GenerateEncry.java
@@ -0,0 +1,16 @@
+package com.currency.configserver;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+public class GenerateEncry {
+    public static void main(String[] args) {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        standardPBEStringEncryptor.setPassword("currency");
+        standardPBEStringEncryptor.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
+        String result = standardPBEStringEncryptor.encrypt("jdbc:h2:mem:bankdb");
+        System.out.println(result);
+        System.out.println(standardPBEStringEncryptor.decrypt(result));
+    }
+}
